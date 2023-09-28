dde5e4fb44e9b8418f7bb647004fed4b2433749b
Rafael Luz <rafaellladm@gmail.com>
2023-09-28 15:09:28 -0300

    iniciando banco de dados sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastfy from 'fastify'
+import { knex } from './database'
 
 const app = fastfy()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
