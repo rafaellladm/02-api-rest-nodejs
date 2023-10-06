0c33fd5bdca223013f1c62e0f6a461ef94d9225b
Rafael Luz <rafaellladm@gmail.com>
2023-10-06 15:36:19 -0300

    Rotas - criação cookin de sessionId

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+import cookie from '@fastify/cookie'
 import fastfy from 'fastify'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastfy()
 
+app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
